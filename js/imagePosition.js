231bd4953c2f7b6cc5cc7b1537efc1844b218887
Luca Lischetti <luca@sirlisko.com>
2016-05-23 13:38:58 +0100

    added support for mobile

--- js/imagePosition.js
+++ js/imagePosition.js
@@ -1,22 +1,32 @@
+const eventsMap = {
+  desktop: { start: 'onmousedown', move: 'onmousemove', end: 'onmouseup' },
+  mobile: { start: 'ontouchstart', move: 'ontouchmove', end: 'ontouchend' }
+}
+
+function eventsPerDevice () {
+  return typeof window.orientation !== undefined ? eventsMap.mobile : eventsMap.desktop
+}
+
 function moveImg (evt) {
   const target = evt.target
-  const diffX = evt.clientX - parseInt(target.style.marginLeft || 0, 10)
-  const diffY = evt.clientY - parseInt(target.style.marginTop || 0, 10)
+  const diffX = evt.pageX - parseInt(target.style.marginLeft || 0, 10)
+  const diffY = evt.pageY - parseInt(target.style.marginTop || 0, 10)
 
   return e => {
-    target.style.marginLeft = `${e.clientX - diffX}px`
-    target.style.marginTop = `${e.clientY - diffY}px`
+    e.preventDefault()
+    target.style.marginLeft = `${e.pageX - diffX}px`
+    target.style.marginTop = `${e.pageY - diffY}px`
   }
 }
 
 function imagePosition () {
+  const events = eventsPerDevice()
   const img = document.querySelector('.zoom__box')
-
-  img.onmousedown = e => {
+  img[events.start] = e => {
     if (e.target.nodeName !== 'IMG') return
 
-    document.onmouseup = () => { document.onmousemove = null }
-    document.onmousemove = moveImg(e)
+    document[events.end] = () => { document[events.move] = null }
+    document[events.move] = moveImg(e)
   }
 }
 
